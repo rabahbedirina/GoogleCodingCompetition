c970de0c5285af64cdbc346f0387c32359561917
Rabah Bedirina <bedirina.rabah1993@gmail.com>
2020-04-04 23:11:37 +0100

    add a for loop to remove ()

--- NestingDepthCodeJam2020.py
+++ NestingDepthCodeJam2020.py
@@ -4,13 +4,22 @@ T = int(input())
 for i in range(1, T+1):
     s = (str(input().strip().replace("(", ""))).replace(")", "")
     y = ""
-    for j in s:
+    for j in range(len(s)):
 
-        if j != "0" and not j.isspace() and j != "":
-            n = int(j)
-            c = "("*n+j+")"*n
+        if s[j] != "0" and not s[j].isspace() and s[j] != "":
+            n = int(s[j])
+            c = "("*n+s[j]+")"*n
         else:
-            c = j
+            c = s[j]
         y += c
-    y = y.replace(")(", "")
+    count = 0
+
+    for p in range(y.find("("), len(y)):
+
+        if y[p] == "(":
+            count += 1
+        else:
+            break
+    for k in range(count):
+        y = y.replace(")(", "")
     print("Case #{}: {} ".format(i, y))
