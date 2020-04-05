5fa14a6eb356f2feaf3aae6099f90c00bbfdc292
Rabah Bedirina <bedirina.rabah1993@gmail.com>
2020-04-05 11:48:19 +0100

    Update Nesting Depth Solution

--- NestingDepthCodeJam2020.py
+++ NestingDepthCodeJam2020.py
@@ -1,25 +1,20 @@
+def main():
+    s = input()
+    y, l = '', 0
+    for i in s:
+        i = int(i)
+        for j in range(i-l):
+            y += '('
+        for j in range(l-i):
+            y += ')'
+        y += str(i)
+        l = i
+    for i in range(l):
+        y += ')'
+    return y
 
 
-T = int(input())
-for i in range(1, T+1):
-    s = (str(input().strip().replace("(", ""))).replace(")", "")
-    y = ""
-    for j in range(len(s)):
+t = int(input())
 
-        if s[j] != "0" and not s[j].isspace() and s[j] != "":
-            n = int(s[j])
-            c = "("*n+s[j]+")"*n
-        else:
-            c = s[j]
-        y += c
-    count = 0
-
-    for p in range(y.find("("), len(y)):
-
-        if y[p] == "(":
-            count += 1
-        else:
-            break
-    for k in range(count):
-        y = y.replace(")(", "")
-    print("Case #{}: {} ".format(i, y))
+for i in range(t):
+    print("Case #{}: {}".format(i+1, main()))
