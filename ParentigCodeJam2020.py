79b75df7ad6d9702358852c50a5a719c7a838b30
Rabah Bedirina <bedirina.rabah1993@gmail.com>
2020-04-05 12:35:35 +0100

    Updating Parenting Partnering Returns Solution

--- ParentigCodeJam2020.py
+++ ParentigCodeJam2020.py
@@ -1,20 +1,29 @@
-
-T = int(input())
-for i in range(1, T+1):
-    N = int(input())
-    S = []
-    E = []
-    y = ""
-    for j in range(N):
-        a, b = map(int, input().split())
-        S.append(a)
-        E.append(b)
-    for p in range(N):
-        for k in range(p + 1, N):
-            if S[p] <= S[k] and S[k] < E[p]:
-                # print("yes")
-                y += "C"
+def main():
+    n = int(input())
+    things = [list(map(int, input().split())) for i in range(n)]
+    print(things)
+    things = [[things[i][0], things[i][1], i] for i in range(n)]
+    print(things)
+    things.sort(key=lambda x: x[0])
+    print(things)
+    a, b = -1, -1
+    res = [None for i in range(n)]
+    print(res)
+    for i in range(n):
+        if things[i][0] < a:
+            if things[i][0] < b:
+                return 'IMPOSSIBLE'
             else:
-                # print("NO")
-                y += "J"
-    print("Case #{}: {}".format(i, y))
+                b = max(b, things[i][1])
+                res[things[i][2]] = 'J'
+        else:
+            a = max(a, things[i][1])
+            res[things[i][2]] = 'C'
+    print(res)
+    return ''.join(res)
+
+
+t = int(input())
+
+for i in range(t):
+    print("Case #{}: {}".format(i+1, main()))
